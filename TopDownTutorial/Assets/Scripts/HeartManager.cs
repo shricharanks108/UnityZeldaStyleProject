aed5e25bbee0ba54e8762731af5c9b5849622ec1
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2020-08-14 08:30:18 -0400

    Last Updated - Aug 14 2020

---
+++ TopDownTutorial/Assets/Scripts/HeartManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour
+{
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite threeQuarterHeart;
+    public Sprite halfHeart;
+    public Sprite quarterHeart;
+    public Sprite emptyHeart;
+    public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
+
+    void Start()
+    {
+        InitHearts();
+    }
+
+    public void InitHearts()
+    {
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+            hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.RuntimeValue / 4;
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            float currHeart = Mathf.Ceil(tempHealth - 1);
+
+            if (i <= tempHealth - 1)
+            {
+                //FullHeart
+                hearts[i].sprite = fullHeart;
+            }
+            else if (i >= tempHealth)
+            {
+                //emptyHeart
+                hearts[i].sprite = emptyHeart;
+            }
+            else if (i == currHeart && (tempHealth % 1) == .50)
+            {
+                //Half full heart
+                hearts[i].sprite = halfHeart;
+            }
+            else if (i == currHeart && (tempHealth % 1) == .25)
+            {
+                //1/4 heart
+                hearts[i].sprite = quarterHeart;
+            }
+            else if (i == currHeart && (tempHealth % 1) == .75)
+            {
+                //3/4 heart
+                hearts[i].sprite = threeQuarterHeart;
+            }
+        }
+    }
+}
---
+++ TopDownTutorial/Assets/Scripts/Log.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    private Rigidbody2D myRigidBody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                anim.SetFloat("moveX", -(transform.position.x - target.position.x));
+                anim.SetFloat("moveY", -(transform.position.y - target.position.y));
+                myRigidBody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
+            }
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    //private void SetAnimFloat(Vector2 setVector)
+    //{
+    //    anim.SetFloat("moveX", setVector.x);
+    //    anim.SetFloat("moveY", setVector.y);
+    //}
+
+    //private void changeAnim(Vector2 direction)
+    //{
+    //    direction = direction.normalized;
+    //    anim.SetFloat("moveX", direction.x);
+    //    anim.SetFloat("moveY", direction.y);
+    //}
+
+    private void ChangeState (EnemyState newState)
+    {
+        if (currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+}
